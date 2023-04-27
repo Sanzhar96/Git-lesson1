94b6ac6d268ec3206c6a25bcbfc94b726f871ad8
Sanzhar <seidaly.sm@gmail.com>
2023-04-27 22:30:13 +0600

    Created new project

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        mainMethodNumber1();
+        mainMethodShowInfo();
+    }
+        private static void mainMethodNumber1(){
+            System.out.println("Hello Able Academy\n");
+        }
+    private static void mainMethodShowInfo(){
+
+        System.out.println("Lets learn Git");
+    }
+    }
